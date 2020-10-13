7538f5df7878184f72476e545f5b66dc5f1fe9d6
Huỳnh Tấn Thịnh <htthinh1999@gmail.com>
2020-10-13 15:11:10 +0700

    Init UI

---
+++ app/src/main/java/com/keycodemon/moneymanager/ItemDetailActivity.java
@@ -0,0 +1,14 @@
+package com.keycodemon.moneymanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ItemDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_detail);
+    }
+}
